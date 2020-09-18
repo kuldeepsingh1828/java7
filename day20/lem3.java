7571949ac754c7ca97383e0e30126f52780cb8a3
kuldeepsingh1828@gmail.com <kuldeepsingh1828@gmail.com>
2020-09-18 19:38:58 +0530

    upto day 19 (2)

---
+++ day20/lem3.java
@@ -0,0 +1,17 @@
+/*
+Lembda Expression
+*/
+interface L
+{
+	int m(int num); //public abstract void first();
+}
+class Main
+{
+	public static void main(String[] args) 
+	{
+		L obj = (int num)->{	
+			return num*num;
+			};	
+			System.out.println("square is : "+ obj.m(5));
+	}
+}
