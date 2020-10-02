fabded8f3139bed6fce10b719a343ef75bcd7129
dewansh3255 <61582764+dewansh3255@users.noreply.github.com>
2020-10-02 17:22:13 +0530

    Update str5.java

--- day24/str5.java
+++ day24/str5.java
@@ -2,7 +2,7 @@ class A
 {
 	public static void main(String[] args) 
 	{
-		String a  = "Hi This            is         Kuldeep        ";
+		String a  = "Hi This            is         Kuldeep        ";		//spaces between characters would not be trimmed
 		System.out.println(a+ " "+a.length());		
 		String b = a.trim().substring(5,12).toUpperCase();
 		System.out.println(a+ " "+a.length());		
